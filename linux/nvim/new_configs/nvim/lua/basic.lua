6fd9418b14dbae933c9ef64225115bba9429d83a
silascript <silascript@outlook.com>
2024-02-22 03:12:26 +0800

    添加新的使用lua方式配置的neovim配置

---
+++ linux/nvim/new_configs/nvim/lua/basic.lua
@@ -0,0 +1,45 @@
+
+
+--
+-- 基础配置
+-- 
+
+-- 显示行号
+vim.opt.number = true
+-- 显示相对行号
+vim.opt.relativenumber = true
+-- 高亮当前行
+vim.opt.cursorline = true
+-- 状态栏样式
+-- vim.opt.laststatus = 2
+
+-- 开启真彩
+vim.opt.termguicolors = true
+
+-- 与底部保持固定间距
+vim.opt.scrolloff=5
+
+
+-- Tab 相关设置
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+
+
+-- 编码
+vim.opt.langmenu = 'zh_CN.UTF-8'
+vim.opt.fileencodings = 'utf-8,gb18030,gb2312,ucs-bom,cp936,big5,euc-jp,euc-kr'
+
+
+-- 搜索
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+
+
+
+
+
