22d017fb797398869d3c350be32413492d9480b9
silascript <silascript@outlook.com>
2024-03-05 11:53:46 +0800

    添加formatter.nvim插件并设置

--- linux/neovim/new_configs/nvim/lua/plugins/basic_plugins.lua
+++ linux/neovim/new_configs/nvim/lua/plugins/basic_plugins.lua
@@ -1,67 +1,57 @@
-
-
 -- 基础的插件集
 
 return {
-
-	--成对符号自动补全
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = function()
-			require("nvim-autopairs").setup {}
-		end	
-
-	},
-
-	-- nvim-cursorline
-	{
-		"yamatsum/nvim-cursorline",
-		config = function()
-			require('nvim-cursorline').setup({
-				cursorword = {
-					enable = true,
-					min_length = 2,
-					hl = { underline = true },
-				  },
-				cursorline = {
-					enable = false,
-				}
-			})
-		end,
-	},
-	
-	-- nvim-surround
-	{
-		"kylechui/nvim-surround",
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end
-	},
-
-	-- comment
-	{
-		'numToStr/Comment.nvim',
-		config = function()
-			require('Comment').setup({
-
-				-- NORMAL模式
-				toggler = {
-					line = '<leader>cc',
-				 },
-				
-				-- NORMAL模式及VISUAL模式
-				opleader = {
-					line = '<leader>cc',
-				},
-
-			})
-		end,
-	},
-
+    --成对符号自动补全
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = function()
+            require("nvim-autopairs").setup {}
+        end
+    },
+    -- nvim-cursorline
+    {
+        "yamatsum/nvim-cursorline",
+        config = function()
+            require("nvim-cursorline").setup(
+                {
+                    cursorword = {
+                        enable = true,
+                        min_length = 2,
+                        hl = {underline = true}
+                    },
+                    cursorline = {
+                        enable = false
+                    }
+                }
+            )
+        end
+    },
+    -- nvim-surround
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({})
+        end
+    },
+    -- comment
+    {
+        "numToStr/Comment.nvim",
+        config = function()
+            require("Comment").setup(
+                {
+                    -- NORMAL模式
+                    toggler = {
+                        line = "<leader>cc"
+                    },
+                    -- NORMAL模式及VISUAL模式
+                    opleader = {
+                        line = "<leader>cc"
+                    }
+                }
+            )
+        end
+    },
 }
-
