5ed4a69ca1aa398a137a322f7083a4c794dd1540
silascript <silascript@outlook.com>
2024-03-15 10:52:40 +0800

    为nvim添加marks.nvim插件

--- linux/neovim/new_configs/nvim/lua/plugins/ui/ui_ex_plugins.lua
+++ linux/neovim/new_configs/nvim/lua/plugins/ui/ui_ex_plugins.lua
@@ -29,4 +29,26 @@ return {
         event = {"VeryLazy"},
         opts = {}
     },
+    -- marks.nvim
+    {
+        "chentoast/marks.nvim",
+        config = function()
+            require("marks").setup(
+                {
+                    -- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
+                    -- sign/virttext. Bookmarks can be used to group together positions and quickly move
+                    -- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
+                    -- default virt_text is "".
+                    bookmark_0 = {
+                        sign = "⚑",
+                        virt_text = "hello world",
+                        -- explicitly prompt for a virtual line annotation when setting a bookmark from this group.
+                        -- defaults to false.
+                        annotate = false
+                    },
+                    mappings = {}
+                }
+            )
+        end
+    }
 }
