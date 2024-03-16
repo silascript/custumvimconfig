e7f192aa9d9c67c93f121165c2cb48dcfb0cb083
silascript <silascript@outlook.com>
2024-03-16 21:40:58 +0800

    添加lorem插件

--- linux/neovim/new_configs/nvim/lua/plugins/utils_plugins.lua
+++ linux/neovim/new_configs/nvim/lua/plugins/utils_plugins.lua
@@ -1,16 +1,32 @@
 -- 小工具的插件集
 
 return {
-
-	-- which-key
+    -- which-key
     {
         "folke/which-key.nvim",
-		enabled =false,
+        enabled = false,
         event = "VeryLazy",
         init = function()
             vim.o.timeout = true
             vim.o.timeoutlen = 300
         end,
         opts = {}
+    },
+    {
+        "derektata/lorem.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("lorem").setup(
+                {
+                    -- 句子长度
+                    -- mixed mixedLong mixedShort long medium short
+                    sentenceLength = "medium",
+                    -- 逗号出现的频率
+                    -- 0~1
+                    -- 0 禁止逗号；1 是三个单词一个逗号
+                    comma = 0.2
+                }
+            )
+        end
     }
 }
