806909f37ebb5dbeeff82604c7db16dcf34659f9
silascript <silascript@outlook.com>
2024-04-04 23:53:24 +0800

    添加lsp-progress插件并配置，以在状态栏显示lsp相关信息

--- linux/neovim/new_configs/nvim/lua/plugins/ui/ui_sl_plugins.lua
+++ linux/neovim/new_configs/nvim/lua/plugins/ui/ui_sl_plugins.lua
@@ -4,15 +4,24 @@ return {
     {
         "nvim-lualine/lualine.nvim",
         dependencies = {"nvim-tree/nvim-web-devicons"},
-		lazy = true,
-		event = { "BufReadPost", "BufAdd", "BufNewFile" },
-		-- event = {"VimEnter"},
-		-- event = {"UIEnter"},
-		-- event = {"VeryLazy"},
+        lazy = true,
+        event = {"BufReadPost", "BufAdd", "BufNewFile"},
+        -- event = {"VimEnter"},
+        -- event = {"UIEnter"},
+        -- event = {"VeryLazy"},
         -- enabled = false,
         enabled = true,
         config = function()
             require("lualine").setup {
+                sections = {
+                    lualine_c = {
+                        "filename",
+                        -- lsp-progress
+                        function()
+                            return require("lsp-progress").progress()
+                        end
+                    }
+                },
                 options = {
                     -- globalstatus = true,
 
@@ -38,9 +47,86 @@ return {
                     -- refresh = {                  -- sets how often lualine should refresh it's contents (in ms)
                     -- 	statusline = 1000,
                     -- },
-                }
+                } -- options
             }
+
+            -- 监听 lsp-progress 事件并刷新 lualine
+            vim.api.nvim_create_augroup("lualine_augroup", {clear = true})
+            vim.api.nvim_create_autocmd(
+                "User",
+                {
+                    group = "lualine_augroup",
+                    pattern = "LspProgressStatusUpdated",
+                    callback = require("lualine").refresh
+                }
+            )
         end
+    },
+    -- lsp-progress.nvim
+    {
+        "linrongbin16/lsp-progress.nvim",
+        lazy = true,
+        event = {"BufReadPost"},
+        config = function()
+            require("lsp-progress").setup(
+                {
+                    client_format = function(client_name, spinner, series_messages)
+                        if #series_messages == 0 then
+                            return nil
+                        end
+                        return {
+                            name = client_name,
+                            body = spinner .. " " .. table.concat(series_messages, ", ")
+                        }
+                    end,
+                    format = function(client_messages)
+                        --- @param name string
+                        --- @param msg string?
+                        --- @return string
+                        local function stringify(name, msg)
+                            return msg and string.format("%s %s", name, msg) or name
+                        end
+
+                        local sign = "" -- nf-fa-gear \uf013
+                        local lsp_clients = vim.lsp.get_active_clients()
+                        local messages_map = {}
+                        for _, climsg in ipairs(client_messages) do
+                            messages_map[climsg.name] = climsg.body
+                        end
+
+                        if #lsp_clients > 0 then
+                            table.sort(
+                                lsp_clients,
+                                function(a, b)
+                                    return a.name < b.name
+                                end
+                            )
+                            local builder = {}
+                            for _, cli in ipairs(lsp_clients) do
+                                if type(cli) == "table" and type(cli.name) == "string" and string.len(cli.name) > 0 then
+                                    if messages_map[cli.name] then
+                                        table.insert(builder, stringify(cli.name, messages_map[cli.name]))
+                                    else
+                                        table.insert(builder, stringify(cli.name))
+                                    end
+                                end
+                            end
+                            if #builder > 0 then
+                                return sign .. " " .. table.concat(builder, ", ")
+                            end
+                        end
+                        return ""
+                    end
+                }
+            )
+        end
+    },
+    -- lualine-lsp-progress
+    {
+        "arkav/lualine-lsp-progress",
+        lazy = true,
+        enabled = false,
+        event = {"BufReadPost"}
     },
     -- linefly
     {
@@ -61,19 +147,7 @@ return {
         enabled = false,
         -- enabled = true,
         config = function()
-            require("hardline").setup(
-                {
-                    -- theme = "catppuccin_minimal"
-                    -- theme = "codeschool_dark"
-                    -- theme = "jellybeans"
-                    -- theme = "nord"
-                    -- theme = "nordic"
-                    -- theme = "one"
-                    -- theme = "gruvbox_minimal"
-                    -- theme = "gruvbox"
-                    -- theme = "evergarden"
-                }
-            )
+            require("hardline").setup({})
         end
     },
     -- yasl
@@ -86,5 +160,5 @@ return {
         config = function()
             require("yasl").setup({})
         end
-    },
+    }
 }
